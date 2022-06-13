6411b0131ba8eeae344eb6af49a4d6fe2da1262b
Raktim Bhattacharya <raktim@tamu.edu>
2022-06-13 12:25:07 +0530

    Complete plotting example with Plots.jl.

--- examples/testCode.jl
+++ examples/testCode.jl
@@ -29,46 +29,30 @@ p1 = plot(RobustControl.BodeMagPlot(G,om),title="G1");
 p2 = plot(RobustControl.BodeMagPlot(G,om),title="G2"); 
 display(plot(p1,p2));
 
-
 # Nyquist Plot
 p = RobustControl.NyquistPlot(s,1,1);
 display(p);
 
-# # System Responses
-# figure(4); clf();
-# subplot(2,2,1);
-# t,y = RobustControl.ImpulseResponse(s,1,1);
-# plot(t,y); grid();
-# xlabel("Time (sec)");
-# #ylabel("Impulse Response");
-# title("Impulse Response");tight_layout();
+# System Responses
+t,y = RobustControl.ImpulseResponse(s,1,1);
+p1=plot(t,y,xlabel="Time (s)", title = "Impulse Response",label=:none); 
 
-# subplot(2,2,2);
-# t,y = RobustControl.StepResponse(s,1,1);
-# plot(t,y); grid();
-# xlabel("Time (sec)");
-# #ylabel("Step Response");
-# title("Step Response");tight_layout();
+t,y = RobustControl.StepResponse(s,1,1);
+p2=plot(t,y,xlabel="Time (s)", title = "Step Response",label=:none); 
 
-# subplot(2,2,3);
-# t = Vector(0:0.01:50);
-# u = ones(length(t)) + 0.1*randn(Float64,length(t)); # Step + Disturbance
-# x0 = [0.0;0.0];
-# y = RobustControl.ForcedResponse(s,1,1,t,u,x0);
-# plot(t,y); grid();
-# xlabel("Time (sec)");
-# #ylabel("Time Response to Step+Disturbance");
-# title("Forced Response");tight_layout();
+t = Vector(0:0.01:50);
+u = ones(length(t)) + 0.1*randn(Float64,length(t)); # Step + Disturbance
+x0 = [0.0;0.0];
+y = RobustControl.ForcedResponse(s,1,1,t,u,x0);
+p3 = plot(t,y,xlabel="Time (s)", title = "Forced Response",label=:none); 
 
-# subplot(2,2,4);
-# t = Vector(0:0.01:10);
-# x0 = [1.0;0.0];
-# t1,y1 = RobustControl.InitialResponse(s,x0); # Auto time grid
-# y2 = RobustControl.InitialResponse(s,x0,t); # Specified time grid
-# l1=plot(t1,y1,"b",linewidth=1);
-# l2=plot(t,y2,"r",linestyle=":",linewidth=2);
-# xlabel("Time (sec)");
-# #ylabel("Initial Response");
-# legend([l1[1],l2[1]],["(auto time grid)","(specified time grid)"]);
-# title("Initial Response");
-# grid(); tight_layout();
+x0 = [1.0;0.0];
+# t,y = RobustControl.InitialResponse(s,x0); # Auto time grid
+t = Vector(0:0.01:10);
+y = RobustControl.InitialResponse(s,x0,t); # Specified time grid
+p4 = plot(t,y,xlabel="Time (s)", layout=(1,2), 
+     label = [L"x_1" L"x_2"],title = "Initial Response", yminorgrid=:false,
+     legend=[:topright :bottomright]); 
+
+p = plot(p1,p2,p3,p4,layout = (2,2));
+display(p)
