1647c55c795f7a1b315f695f5b0507a57a85f8b7
Raktim Bhattacharya <raktim@tamu.edu>
2022-06-13 10:55:29 +0530

    Changed from PyPlot to Plots

--- examples/testCode.jl
+++ examples/testCode.jl
@@ -1,5 +1,5 @@
 # Code to test basic functionality and plotting
-using RobustControl, PyPlot
+using RobustControl, Plots
 
 A = [0 1;-1 -1];
 B = reshape([0;1],2,1);
@@ -20,72 +20,68 @@ om1 = RobustControl.logspace(1E-2,1E2,200);
 G1 = RobustControl.FrequencyResponse(s,2,1,om1);
 
 # Test plots
-figure(1);clf(); pygui(true);
-subplot(2,1,1);
-RobustControl.BodeMagPlot(G,om); title("Bode Mag Plot");
-RobustControl.BodeMagPlot(G1,om1);
-grid(which="both",color=[1,1,1]*0.9,linewidth=0.35)
-legend(["G1","G2"]);
-tight_layout();
+p1 = RobustControl.BodeMagPlot(G,om); title("Bode Mag Plot");
+p2 = RobustControl.BodeMagPlot(G1,om1);
 
-# Another way to get Bode plots -- just magnitude.
-subplot(2,1,2);
-RobustControl.BodeMagPlot(s,1,1);
-RobustControl.BodeMagPlot(s,2,1);
-grid(which="both",color=[1,1,1]*0.9,linewidth=0.35)
-legend(["G1","G2"]);
-tight_layout();
 
-# 3rd way -- get both mag and phase in one plot
-figure(2); clf();
-RobustControl.BodePlot(s,1,1);
-RobustControl.BodePlot(s,2,1);
-subplot(211);grid(which="both",color=[1,1,1]*0.9,linewidth=0.35)
-subplot(212);grid(which="both",color=[1,1,1]*0.9,linewidth=0.35)
-subplot(211);legend(["G1","G2"]);
-title("Bode Plot");
-tight_layout();
+# # Another way to get Bode plots -- just magnitude.
+# subplot(2,1,2);
+# RobustControl.BodeMagPlot(s,1,1);
+# RobustControl.BodeMagPlot(s,2,1);
+# grid(which="both",color=[1,1,1]*0.9,linewidth=0.35)
+# legend(["G1","G2"]);
+# tight_layout();
 
-# Nyquist Plot
-figure(3);clf();
-subplot(2,1,1);RobustControl.NyquistPlot(s,1,1); title("Nyquist Plot"); # With auto frequency grid
-subplot(2,1,2);RobustControl.NyquistPlot(s,1,1,om1); # With specified frequency grid
+# # 3rd way -- get both mag and phase in one plot
+# figure(2); clf();
+# RobustControl.BodePlot(s,1,1);
+# RobustControl.BodePlot(s,2,1);
+# subplot(211);grid(which="both",color=[1,1,1]*0.9,linewidth=0.35)
+# subplot(212);grid(which="both",color=[1,1,1]*0.9,linewidth=0.35)
+# subplot(211);legend(["G1","G2"]);
+# title("Bode Plot");
+# tight_layout();
 
-# System Responses
-figure(4); clf();
-subplot(2,2,1);
-t,y = RobustControl.ImpulseResponse(s,1,1);
-plot(t,y); grid();
-xlabel("Time (sec)");
-#ylabel("Impulse Response");
-title("Impulse Response");tight_layout();
+# # Nyquist Plot
+# figure(3);clf();
+# subplot(2,1,1);RobustControl.NyquistPlot(s,1,1); title("Nyquist Plot"); # With auto frequency grid
+# subplot(2,1,2);RobustControl.NyquistPlot(s,1,1,om1); # With specified frequency grid
 
-subplot(2,2,2);
-t,y = RobustControl.StepResponse(s,1,1);
-plot(t,y); grid();
-xlabel("Time (sec)");
-#ylabel("Step Response");
-title("Step Response");tight_layout();
+# # System Responses
+# figure(4); clf();
+# subplot(2,2,1);
+# t,y = RobustControl.ImpulseResponse(s,1,1);
+# plot(t,y); grid();
+# xlabel("Time (sec)");
+# #ylabel("Impulse Response");
+# title("Impulse Response");tight_layout();
 
-subplot(2,2,3);
-t = Vector(0:0.01:50);
-u = ones(length(t)) + 0.1*randn(Float64,length(t)); # Step + Disturbance
-x0 = [0.0;0.0];
-y = RobustControl.ForcedResponse(s,1,1,t,u,x0);
-plot(t,y); grid();
-xlabel("Time (sec)");
-#ylabel("Time Response to Step+Disturbance");
-title("Forced Response");tight_layout();
+# subplot(2,2,2);
+# t,y = RobustControl.StepResponse(s,1,1);
+# plot(t,y); grid();
+# xlabel("Time (sec)");
+# #ylabel("Step Response");
+# title("Step Response");tight_layout();
 
-subplot(2,2,4);
-t = Vector(0:0.01:10);
-x0 = [1.0;0.0];
-t1,y1 = RobustControl.InitialResponse(s,x0); # Auto time grid
-y2 = RobustControl.InitialResponse(s,x0,t); # Specified time grid
-l1=plot(t1,y1,"b",linewidth=1);
-l2=plot(t,y2,"r",linestyle=":",linewidth=2);
-xlabel("Time (sec)");
-#ylabel("Initial Response");
-legend([l1[1],l2[1]],["(auto time grid)","(specified time grid)"]);
-title("Initial Response");
-grid(); tight_layout();
+# subplot(2,2,3);
+# t = Vector(0:0.01:50);
+# u = ones(length(t)) + 0.1*randn(Float64,length(t)); # Step + Disturbance
+# x0 = [0.0;0.0];
+# y = RobustControl.ForcedResponse(s,1,1,t,u,x0);
+# plot(t,y); grid();
+# xlabel("Time (sec)");
+# #ylabel("Time Response to Step+Disturbance");
+# title("Forced Response");tight_layout();
+
+# subplot(2,2,4);
+# t = Vector(0:0.01:10);
+# x0 = [1.0;0.0];
+# t1,y1 = RobustControl.InitialResponse(s,x0); # Auto time grid
+# y2 = RobustControl.InitialResponse(s,x0,t); # Specified time grid
+# l1=plot(t1,y1,"b",linewidth=1);
+# l2=plot(t,y2,"r",linestyle=":",linewidth=2);
+# xlabel("Time (sec)");
+# #ylabel("Initial Response");
+# legend([l1[1],l2[1]],["(auto time grid)","(specified time grid)"]);
+# title("Initial Response");
+# grid(); tight_layout();
--- src/Plotting.jl
+++ src/Plotting.jl
@@ -1,15 +1,16 @@
-# Plotting routines -- uses Plots
+# Plotting routines -- uses Plots. Not using PyPlot Directly.
 
 using Plots
+# Plot defaults
+default(titlefont = (10, "times"), legendfontsize = 10, guidefont = (8, :black), 
+tickfont = (6, :black), framestyle = :default, minorticks=:false , label=:none, 
+yminorgrid = false, xlabel = "", ylabel = "", linewidth=1, color=:dodgerblue2);
 
 # Bodemag with frsp and om given
 function BodeMagPlot(freqResponse::Vector{ComplexF64},om::Vector{Float64})
-    plot(om,abs.(freqResponse),linewidth=.75);
-    xscale("log");
-    yscale("log");
-    xlabel("Frequency (rad/s)");
-    ylabel("Magnitude");
-    tight_layout();
+    p = plot(om, abs.(freqResponse), xaxis=:log, yaxis=:log, xlabel="Frequency (rad/s)", ylabel = "Magnitude");
+    display(p);
+    return p
 end
 
 # Bodemag directly from system
@@ -22,23 +23,23 @@ function BodeMagPlot(sys::StateSpace,no::Integer, ni::Integer, om::Vector{Float6
     else
         freqResponse = FrequencyResponse(sys,no,ni,om);
     end
-    BodeMagPlot(freqResponse,om);
+    return BodeMagPlot(freqResponse,om);
 end
 
 # Bode phase with frsp and om given -- this method should be sufficient. Other methods are not necessary.
 function BodePhasePlot(freqResponse::Vector{ComplexF64},om::Vector{Float64})
     phase = 180*atan.(imag.(freqResponse),real.(freqResponse))/pi; # Convert it to [-180,180]
-    plot(om,phase,linewidth=.75);
-    xscale("log");
-    #yscale("log");
-    xlabel("Frequency (rad/s)");
-    ylabel("Phase");
-    tight_layout();
+    p = plot(om, phase, xaxis=:log, yaxis=:log, xlabel="Frequency (rad/s)", ylabel = "Phase (deg)");
+    display(p);
+    return p;
 end
 
 function BodePlot(freqResponse::Vector{ComplexF64},om::Vector{Float64})
-    subplot(211); BodeMagPlot(freqResponse,om);
-    subplot(212); BodePhasePlot(freqResponse,om);
+    p1 = BodeMagPlot(freqResponse,om);
+    p2 = BodePhasePlot(freqResponse,om);
+    p = plot(p1,p2); # Need to check dimension and layout.
+    display(p);
+    return p;
 end
 
 # Bodemag directly from system
@@ -65,12 +66,11 @@ function NyquistPlot(sys::StateSpace,no::Integer, ni::Integer, om::Vector{Float6
         f1 = FrequencyResponse(sys,no,ni,om);
         f2 = FrequencyResponse(sys,no,ni,-om);
     end
-    plot(real.(f1),imag.(f1),linewidth=0.5); # Positive ω
-    plot(real.(f2),imag.(f2),linewidth=0.5); # Negative ω
-    grid(which="both",color=[1,1,1]*0.9,linewidth=0.25)
-    xlabel("Real");
-    ylabel("Imaginary");
-    tight_layout();
+    p = plot(real.(f1),imag.(f1),color=:red); # Positive ω
+    plot!(real.(f2),imag.(f2)); # Negative ω
+    plot!(grid=:true,grid_lw=0.5, xlabel="Real", "Imaginary");
+    display(p);
+    return p
 end
 
 function PoleZeroMap()
